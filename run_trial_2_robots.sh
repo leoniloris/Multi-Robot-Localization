ef29d4e9772b0e96e0d66570157455ad6454a1ee
Leoni Mota Loris <leoniloris1@hotmail.com>
2021-05-16 18:21:06 -0300

    Run test trials on several runs/

--- run_trial_2_robots.sh
+++ run_trial_2_robots.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 set -e
 
-[ -z ${TRIAL+x} ] && echo "Needs to define the variable TRIAL" && exit 1
-
 function kill_processes {
-    sudo pkill -f robot_node && sudo pkill -f wall_following
+    sudo pkill -f robot_node
+    sudo pkill -f wall_following
+    sudo pkill -f gazebo
+    sudo pkill -f python3
+    sudo pkill -f noetic
+}
+
+function run_trial {
+    roslaunch -v multi_robot_localization main.launch n_robots:=2 &
+    sleep 5
+
+    rosrun multi_robot_localization robot_node 1 & \
+    python3 src/multi_robot_localization/scripts/wall_following.py 1 & \
+    rosrun multi_robot_localization robot_node 2 & \
+    python3 src/multi_robot_localization/scripts/wall_following.py 2 &
 }
 
 trap kill_processes SIGINT
 
-rosrun multi_robot_localization robot_node 1 & \
-python3 src/multi_robot_localization/scripts/wall_following.py 1 & \
-rosrun multi_robot_localization robot_node 2 & \
-python3 src/multi_robot_localization/scripts/wall_following.py 2
+for TRIAL in {1..5}; do
+    echo "==========================================================="
+    echo "================ Running trial ${TRIAL}... ================"
+    echo "==========================================================="
+    run_trial
+    sleep 300
+    kill_processes
+done
