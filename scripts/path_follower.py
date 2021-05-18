dcc6fcb643940f6f10628d74646375b82a1a942b
Leoni Mota Loris <leoniloris1@hotmail.com>
2021-05-18 18:53:16 -0300

    scructure for controlling the robot.

--- scripts/path_follower.py
+++ scripts/path_follower.py
@@ -1,19 +1,36 @@
-# import ros stuff
 import numpy as np
 import rospy
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import (Twist, Quaternion)
+from geometry_msgs.msg import Twist
 from tf import transformations
 from nav_msgs.msg import Odometry
 
-actuator_publisher = None
-path = [(10, 20), (25, 20), (25, 62), (115, 62), (115, 105), (130, 105)]
+with open(os.environ["HOME"] + "/catkin_ws/src/multi_robot_localization/include/occupancy_grid.h", mode='r') as occupancy_grid:
+    text = occupancy_grid.read()
+    X_CENTER = float(re.findall(r'X_CENTER \((.*?)\)', text)[0])
+    Y_CENTER = float(re.findall(r'Y_CENTER \((.*?)\)', text)[0])
+    CELLS_PER_METER = float(re.findall(r'CELLS_PER_METER \((.*?)\)', text)[0])
 
+actuator_publisher = None
+path_landmarks = [(10, 20), (25, 20), (25, 62), (115, 62), (115, 105), (130, 105)]
+MIN_LANDMARK_DISTANCE = 3
 
 def clbk_odometry(msg):
-    angle = transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
-                                                   msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])[2]
-    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", angle*180/np.pi)
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+    angle = transformations.euler_from_quaternion([
+        msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])[2]
+    # 1- check if I can clear a landmark (and then reduce speed a little)
+    # 2- compute error until the next landmark
+    # 3- change angle aiming to reach the next (reduce speed a little)
+    # 4- go to the next landmark full speed
+    # 5- 1
+
+    control_msg = Twist()
+    control_msg.linear.x = 0.2
+    control_msg.angular.z = -0.4
+
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", x, y, angle*180/np.pi)
 
 
 def main():
@@ -22,18 +39,14 @@ def main():
     global actuator_publisher
 
     rospy.init_node(f'path_follower_{robot_suffix}')
-    laser_topic = '/ugv' + str(robot_suffix) + '/scan'
     odometry_topic = '/ugv' + str(robot_suffix) + '/odom'
     vel_topic = '/ugv' + str(robot_suffix) + '/cmd_vel'
-    print(laser_topic)
     print(vel_topic)
 
     actuator_publisher = rospy.Publisher(vel_topic, Twist, queue_size=1)
-
     sub = rospy.Subscriber(odometry_topic, Odometry, clbk_odometry)
 
-    rate = rospy.Rate(20)
-
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         rate.sleep()
 
